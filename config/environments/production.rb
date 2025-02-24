b740fac738935619ed626e823efd30556f98edaa
NebulaBC <contact@nebulabc.net>
2025-02-24 16:02:08 -0500

    no keys please

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  config.require_master_key = false
+  config.read_encrypted_secrets = false
+
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
